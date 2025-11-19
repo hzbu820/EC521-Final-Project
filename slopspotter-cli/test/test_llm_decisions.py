1e56548ae32adbdfe42a6bb70b135d4859106175
Victor Mercola <victormerc705@gmail.com>
2025-11-19 18:00:39 -0500

    Add graph tests & edge labels when drawing decision trees

--- slopspotter-cli/test/test_llm_decisions.py
+++ slopspotter-cli/test/test_llm_decisions.py
@@ -1,28 +1,44 @@
 import unittest
 
+import matplotlib.pyplot as plt
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from slopspotter.llm_decisions import topk_token_probabilities
+from slopspotter.llm_decisions import (
+    draw_decision_tree,
+    token_decision_tree,
+    topk_token_probabilities,
+)
 
 
 class TestLLMDecisions(unittest.TestCase):
     """Test suite for calculating LLM token probabilities."""
 
+    model = AutoModelForCausalLM.from_pretrained("gpt2", device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2", device_map="auto")
+
     def test_topk_token_probabilities(self):
         """Test calculations for top-k next tokens."""
 
-        model = AutoModelForCausalLM.from_pretrained("gpt2", device_map="auto")
-        tokenizer = AutoTokenizer.from_pretrained("gpt2", device_map="auto")
         input_text = "The quick brown fox jumps over the lazy"
         top_k_probabilities, top_k_token_ids = topk_token_probabilities(
-            model, tokenizer, input_text
+            self.model, self.tokenizer, input_text
         )
 
         print(input_text + "...")
         for prob, token_id in zip(top_k_probabilities, top_k_token_ids, strict=True):
             prob_percent = prob * 100
-            token = tokenizer.decode(token_id)
+            token = self.tokenizer.decode(token_id)
             print(f"\t{prob_percent:.2f}%: ID {token_id} ('{token}')")
+
+    def test_token_decision_tree(self):
+        """Test decision tree calculation."""
+
+        input_text = "The quick brown fox jumps over the lazy"
+        decision_tree = token_decision_tree(
+            self.model, self.tokenizer, input_text, k=3, max_depth=2
+        )
+        draw_decision_tree(decision_tree)
+        plt.savefig("decision_tree.png")
 
 
 if __name__ == "__main__":
