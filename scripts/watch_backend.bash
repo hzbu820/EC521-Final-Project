a90afa893791a3e6f86cde03f13a0a8cd41f4366
Victor Mercola <victormerc705@gmail.com>
2025-12-02 19:08:12 -0500

    update backend watching script

--- scripts/watch_backend.bash
+++ scripts/watch_backend.bash
@@ -4,4 +4,4 @@
 ## @author Victor Mercola
 ## @date 2025-11-30
 ## @brief View `slopspotter-cli`'s log as it updates on Linux
-watch -n 2 tail --lines=20 slopspotter-cli/log.log
+watch -n 2 tail slopspotter-cli/log.log
